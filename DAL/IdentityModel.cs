f8a22ff717d000f1b838d474c877218032572bc5
ShadyShawkat <shadyshawkat001@gmail.com>
2021-03-16 15:38:52 +0200

    Hi

--- DAL/IdentityModel.cs
+++ DAL/IdentityModel.cs
@@ -12,7 +12,7 @@ namespace DAL
     public class ApplicationUserIdentity : IdentityUser
     {
         //additional fields
-
+        ////////
     }
     public class ApplicationUserStore : UserStore<ApplicationUserIdentity>
     {
